579987e9cda8f6e411c3714382cdb09e7cae5a52
Pourya <pourya.nejadensan@gmail.com>
2023-08-10 22:08:51 +0200

    Creating JPA Entity - Comment.

---
+++ src/main/java/com/springboot/blog/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.blog.repository;
+
+import com.springboot.blog.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
