3f8cab6c9d45f2a2060a00a5f8f9449917f0a05f
Pourya <pourya.nejadensan@gmail.com>
2023-08-10 21:53:12 +0200

    Creating JPA Entity - Comment.

---
+++ src/main/java/com/springboot/blog/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(
+        name = "comments"
+)
+public class Comment {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    private String body;
+    private String email;
+    private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id",nullable = false)
+    private Post post;
+}
