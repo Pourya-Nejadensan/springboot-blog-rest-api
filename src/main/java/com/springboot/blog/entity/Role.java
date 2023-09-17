45a7494e445ff102aff3d717b0d5f1d0e18a2cab
Pourya Nejadensan <pourya.nejadensan@gmail.com>
2023-09-17 13:51:46 +0200

    Create JPA Entities - User and Role ( Many to Many Mapping).

---
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -0,0 +1,19 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String name;
+}
